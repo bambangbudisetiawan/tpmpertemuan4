69080fce5dd2c9ce9b1f9fe7596f4bcfa6ce3a42
bambangbudisetiawan <bambangbudisetiawan46@gmail.com>
2020-03-01 19:43:01 +0700

    test

---
+++ TPM-PERTEMUAN-4/app/src/main/java/com/example/pertemuantiga/FragmentBioku.java
@@ -0,0 +1,31 @@
+package com.example.pertemuantiga;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentBioku extends Fragment {
+
+
+    public FragmentBioku() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment_bioku, container, false);
+    }
+
+}
---
+++ TPM-PERTEMUAN-4/app/src/main/java/com/example/pertemuantiga/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.pertemuantiga;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView btmNav;
+    private Fragment selectedFragment = new Fragmentherosku();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btmNav = findViewById(R.id.activitymain_bottomnav);
+
+        btmNav.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(selectedFragment);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.menu_hero:
+                selectedFragment = new Fragmentherosku();
+                break;
+
+            case R.id.menu_Me:
+                selectedFragment = new FragmentBioku();
+                break;
+        }
+        return loadFragment(selectedFragment);
+    }
+
+    private boolean loadFragment(Fragment selectedFragment) {
+        if (selectedFragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.activitymain_container,selectedFragment)
+                    .commit();
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
